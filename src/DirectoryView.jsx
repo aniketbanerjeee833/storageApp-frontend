10b0cab6875bc970a838ea4f1148532ab977495a
Aniket Banerjee <aniketbanerjee8334845906@gmail.com>
2026-06-24 08:56:48 +0530

    chnaged from local

--- src/DirectoryView.jsx
+++ src/DirectoryView.jsx
@@ -53,7 +53,7 @@ function DirectoryView() {
   const loadDirectory = async () => {
     try {
       const data = await getDirectoryItems(dirId);
-      setDirectoryName(dirId ? data.name : "My Home ");
+      setDirectoryName(dirId ? data.name : "My Drive");
       setDirectoriesList([...data.directories].reverse());
       setFilesList([...data.files].reverse());
     } catch (err) {
